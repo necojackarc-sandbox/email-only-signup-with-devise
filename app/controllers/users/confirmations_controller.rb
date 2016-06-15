bfd8c64c810903c72f7951f36c5d15b8eeb591c2
necojackarc <necojackarc@gmail.com>
2016-06-15 16:09:17 +0900

    Fix :bug:
    `resource_params` is defined in the super class so overriding it is harmful

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -19,7 +19,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     confirmation_token = params[resource_name][:confirmation_token]
     self.resource = resource_class.find_by_confirmation_token!(confirmation_token)
 
-    if resource.update(resource_params)
+    if resource.update(confirmation_params)
       self.resource = resource_class.confirm_by_token(confirmation_token)
       set_flash_message :notice, :confirmed
       sign_in_and_redirect resource_name, resource
@@ -42,7 +42,7 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
 
   private
 
-  def resource_params
+  def confirmation_params
     params.require(resource_name).permit(:password)
   end
 end
